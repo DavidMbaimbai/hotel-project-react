ad8921178150e39562d1291c1a696d27594ce4ce
David Mbaimbai <dmbaimbai@csod.com>
2024-02-13 19:07:25 +0200

    Hotel Project with React

--- src/main/java/com/davymbaimbai/zimhotel/model/Room.java
+++ src/main/java/com/davymbaimbai/zimhotel/model/Room.java
@@ -1,15 +1,25 @@
 package com.davymbaimbai.zimhotel.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@Builder
 
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked = false;
-
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room(){
