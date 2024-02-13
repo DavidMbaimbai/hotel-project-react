cded59b7c811eb29d24aba52115928dba6a159f3
David Mbaimbai <dmbaimbai@csod.com>
2024-02-13 20:10:43 +0200

    Hotel Project with React

--- src/main/java/com/davymbaimbai/zimhotel/response/BookingResponse.java
+++ src/main/java/com/davymbaimbai/zimhotel/response/BookingResponse.java
@@ -1,4 +1,34 @@
 package com.davymbaimbai.zimhotel.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingResponse {
+    private Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numberOfAdults;
+    private int numberOfChildren;
+    private int totalNumberOfGuest;
+    private String bookingConfirmationCode;
+
+    private RoomResponse room;
+
+    public BookingResponse(Long bookingId,
+                           LocalDate checkInDate,
+                           LocalDate checkOutDate,
+                           String bookingConfirmationCode) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
