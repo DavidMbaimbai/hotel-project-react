b13ecf2e1aab828c0699c7949fe8034d5b559b50
David Mbaimbai <dmbaimbai@csod.com>
2024-02-13 18:59:24 +0200

    Hotel Project with React

--- src/main/java/com/davymbaimbai/zimhotel/model/BookedRoom.java
+++ src/main/java/com/davymbaimbai/zimhotel/model/BookedRoom.java
@@ -1,9 +1,6 @@
 package com.davymbaimbai.zimhotel.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -17,17 +14,20 @@ public class BookedRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookingId;
+    @Column(name = "check_in")
     private LocalDate checkInDate;
-
+    @Column(name = "check_out")
     private LocalDate checkOutDate;
+    @Column(name = "guest_fullname")
     private String guestFullName;
-
+    @Column(name = "guest_email")
     private String guestEmail;
-
+    @Column(name = "adults")
     private int numberOfAdults;
+    @Column(name = "childern")
     private int numberOfChildren;
     private int totalNumberOfGuest;
-
+    @Column(name = "confirmation_code")
     private String bookingConfirmationCode;
 
     private Room room;
