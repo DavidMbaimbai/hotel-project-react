fd3baac63bc9175da532e5af7cce29e1828c5158
David Mbaimbai <dmbaimbai@csod.com>
2024-02-13 19:29:09 +0200

    Hotel Project with React

--- src/main/java/com/davymbaimbai/zimhotel/response/RoomResponse.java
+++ src/main/java/com/davymbaimbai/zimhotel/response/RoomResponse.java
@@ -1,9 +1,13 @@
 package com.davymbaimbai.zimhotel.response;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.util.List;
-
+@Data
+@NoArgsConstructor
 public class RoomResponse {
     private Long id;
     private String roomType;
@@ -12,4 +16,22 @@ public class RoomResponse {
     private Blob photo;
 
     private List<BookingResponse> bookings;
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType,
+                        BigDecimal roomPrice,
+                        boolean isBooked, Blob photo,
+                        List<BookingResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photo;
+        this.bookings = bookings;
+    }
 }
